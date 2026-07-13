da957a2d922d38a67241dfd28103612adce11a31
DIKSHANT YADAV <andikshantyadav2006@gmail.com>
2026-07-13 14:20:31 +0530

    style: update Nav component for improved layout and accessibility

--- apps/main-web/src/components/navbar/Nav.jsx
+++ apps/main-web/src/components/navbar/Nav.jsx
@@ -31,21 +31,19 @@ const Nav = ({ isDarkMode, toggleDarkMode, showNav, navCardToggleButton, navCard
   return (
     <>
       <nav ref={ref} className="relative w-full px-2 md:px-20 py-2 flex justify-between items-center">
-        <h1 className="text-[2vw] font-['boldtext'] text-black dark:text-white">Dikshant</h1>
-
-        <button onClick={toggleDarkMode} className="px-2 cursor-target cursor-none ">
+        <h1 className="text-[1rem] uppercase font-black font-['boldtext'] text-black dark:text-white">Dikshant</h1>
+        <div  className={`flex cursor-target justify-between min-h-[26px] transition-all items-center gap-1 bg-[--dark-color] dark:bg-[--light-color] ${showNav ? "rounded-sm pr-10" : "pr-2 rounded-lg"} pl-2 py-2`}>
+          {/* <h2 className="text-[2.5vw] sm:text-[1.5vw] font-['normaltext'] text-[--light-color] dark:text-[--dark-color]">
+            
+          </h2> */}
+          <button onClick={toggleDarkMode} className="px-2 cursor-target cursor-none ">
           {isDarkMode ? (
             <img className="p-[5px] rounded-full h-7 bg-[#1d1e20] opacity-70" src={light} alt="light mode icon" />
           ) : (
             <img className="p-[5px] rounded-full h-7 bg-[--dark-color] opacity-70 mix-blend-difference" src={dark} alt="dark mode icon" />
           )}
-        </button>
-
-        <div onClick={navCardToggleButton} className={`flex cursor-target justify-between min-h-[26px] transition-all items-center gap-1 bg-[--dark-color] dark:bg-[--light-color] ${showNav ? "rounded-sm pr-10" : "pr-2 rounded-lg"} pl-2 py-2`}>
-          <h2 className="text-[2.5vw] sm:text-[1.5vw] font-['normaltext'] text-[--light-color] dark:text-[--dark-color]">
-            MENU
-          </h2>
-          <div className={`flex items-center flex-col ${showNav ? "gap-[0px]" : "gap-[4px]"}`}>
+          </button>
+          <div onClick={navCardToggleButton} className={`flex items-center flex-col ${showNav ? "gap-[0px]" : "gap-[4px]"}`}>
             <span ref={(el) => (spanRefs.current[0] = el)} className={`w-8 h-[4px] inline-block bg-[--light-color] dark:bg-[--dark-color] ${showNav ? "rounded-full" : "rounded-none"}`}></span>
             <span ref={(el) => (spanRefs.current[1] = el)} className={`w-8 h-[4px] inline-block bg-[--light-color] dark:bg-[--dark-color] ${showNav ? "rounded-full" : "rounded-none"}`}></span>
             <span ref={(el) => (spanRefs.current[2] = el)} className={`w-8 h-[4px] inline-block bg-[--light-color] dark:bg-[--dark-color] ${showNav ? "rounded-full" : "rounded-none"}`}></span>
@@ -58,7 +56,8 @@ const Nav = ({ isDarkMode, toggleDarkMode, showNav, navCardToggleButton, navCard
             cursorModes={cursorModes}
             showNav={showNav}
             isDarkMode={isDarkMode}
-            navCardToggleButton={navCardToggleButton}/>
+            navCardToggleButton={navCardToggleButton}
+      />
       </div>
       </nav>
     </>
